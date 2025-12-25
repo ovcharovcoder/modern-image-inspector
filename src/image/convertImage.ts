af4784e638b48d76b0fd16f3f96c017001ec852a
Andriy Ovcharov <ovcharovcoder@gmail.com>
2025-12-25 08:56:17 +0200

    Delete src directory

--- src/image/convertImage.ts
+++
@@ -1,9 +0,0 @@
-import sharp from 'sharp';
-
-export async function saveAsWebP(buffer: Buffer, path: string) {
-  await sharp(buffer).webp({ quality: 75 }).toFile(path);
-}
-
-export async function saveAsAvif(buffer: Buffer, path: string) {
-  await sharp(buffer).avif({ quality: 50 }).toFile(path);
-}
