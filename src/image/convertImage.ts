bb070944c57756be6116c114ec3cbf5ea0d1fd29
Andriy Ovcharov <ovcharovcoder@gmail.com>
2025-12-25 11:08:45 +0200

    Update convertImage.ts

--- src/image/convertImage.ts
+++ src/image/convertImage.ts
@@ -1,9 +1,19 @@
 import sharp from 'sharp';
 
-export async function saveAsWebP(buffer: Buffer, path: string) {
-  await sharp(buffer).webp({ quality: 75 }).toFile(path);
+/**
+ * Конвертує PNG або JPG у WebP
+ * @param filePath - шлях до оригінального файлу
+ * @param outputPath - шлях для збереження WebP
+ */
+export async function convertToWebP(filePath: string, outputPath: string) {
+  await sharp(filePath).webp({ quality: 75 }).toFile(outputPath);
 }
 
-export async function saveAsAvif(buffer: Buffer, path: string) {
-  await sharp(buffer).avif({ quality: 50 }).toFile(path);
+/**
+ * Конвертує PNG або JPG у AVIF
+ * @param filePath - шлях до оригінального файлу
+ * @param outputPath - шлях для збереження AVIF
+ */
+export async function convertToAvif(filePath: string, outputPath: string) {
+  await sharp(filePath).avif({ quality: 50 }).toFile(outputPath);
 }
